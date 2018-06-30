9912e2493621476cc5bd806ef0053f0b309de61c
abdelmenem <40714089+abdelmenem@users.noreply.github.com>
2018-06-30 12:33:52 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,23 +12,23 @@
 
 int main()
 {
-	DDRD|=(1<<4);
-	DDRD&=~(1<<2);
-	PORTD|=(1<<4);
-	PORTD|=(1<<2);
-	DDRA&=~(1<<0);
-	INT0_INIT();
-	UART_INIT();
-	ADC_INIT();
+	DDRD|=(1<<4);     // adjust led pin as output
+	DDRD&=~(1<<2);	  // adjust interrupt0 pin as input
+	PORTD|=(1<<4);    // led pin initial value ON
+	PORTD|=(1<<2);    // interrupt pin internal pull up res activate
+	DDRA&=~(1<<0);    // adjust ADC channel 0 as input
+	INT0_INIT();	  // initiate interrupt 0
+	UART_INIT();      // initiate uart with baudrate 9600
+	ADC_INIT();       // initiate ADC
 	int temp;
 	char temperature_string[10];
 	for(;;)
 	{
 
-		temp = ADC_read(0);
-		temp = (temp*150*5)/(1023*1.5);
-		itoa(temp,temperature_string,10);
-		UART_sendString(temperature_string);
-		_delay_ms(3000);
+		temp = ADC_read(0);					// read value of temperature and store it in temp variable
+		temp = (temp*150*5)/(1023*1.5);     //convert received value into temperature value
+		itoa(temp,temperature_string,10);   // convert value to string to send it using uart
+		UART_sendString(temperature_string); // send using uart
+		_delay_ms(3000); 					// delay 3 seconds
 	}
 }
